c55d2346ba02920764e5342f566006ea900e6130
kokou-stm <kokou.sekpona@uphf.fr>
2025-09-04 16:38:54 +0200

    add changes

--- nextvideo/settings.py
+++ nextvideo/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = 'django-insecure-(_r8&+xf5=z(%m55lkj^p52#p87-cna&wf3&44t386iex*(haf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True  
 
-ALLOWED_HOSTS = ["b5ae-46-193-67-154.ngrok-free.app", "aivoicedubber.com","414f-46-193-67-154.ngrok-free.app", "127.0.0.1"]#, ,"localhost","videoadd.fggbdpcqfnguf4ga.eastus.azurecontainer.io", "aivoicedubber.com"]
-CSRF_TRUSTED_ORIGINS =["https://*.aivoicedubber.com","https://*.b5ae-46-193-67-154.ngrok-free.app", "https://b5ae-46-193-67-154.ngrok-free.app"]#,"https://aivoicedubber.com",  "https://.*aivoicedubber.com"]
+ALLOWED_HOSTS = ["voicetovoice.fr", "aivoicedubber.com","414f-46-193-67-154.ngrok-free.app", "127.0.0.1"]#, ,"localhost","videoadd.fggbdpcqfnguf4ga.eastus.azurecontainer.io", "aivoicedubber.com"]
+CSRF_TRUSTED_ORIGINS =["https://*.voicetovoice.fr","https://*.b5ae-46-193-67-154.ngrok-free.app", "https://b5ae-46-193-67-154.ngrok-free.app"]#,"https://aivoicedubber.com",  "https://.*aivoicedubber.com"]
 
 
 
